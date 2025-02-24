2ba47fee6ea7208667383c72601a47ddacb09f6a
Sara Lowenthal <saralowenthal@Saras-MacBook-Pro.local>
2025-02-24 17:24:04 -0500

    Updating html, css, and js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 // Sara Lowenthal 2/24/25
 
 function getCompoundInterest() { 
-    let initialInvestment = parseFloat(document.getElementById('initialInvestment').value); 
-    let annualInterestRate = parseFloat(document.getElementById('annualInterestRate').value); 
-    let retirementYear = parseInt(document.getElementById('retirementYear').value); 
+    const initialInvestment = parseFloat(document.getElementById('initialInvestment').value); 
+    const annualInterestRate = parseFloat(document.getElementById('annualInterestRate').value); 
+    const retirementYear = parseInt(document.getElementById('retirementYear').value); 
 
     // add logic for calculating compound interest 
-    let currentYear = new Date().getFullYear();
+    const currentYear = new Date().getFullYear();
     if (retirementYear <= currentYear) {
         alert("Please enter a valid retirement year in the future.");
     }
-    let yearsInvested = retirementYear - currentYear;
+    const yearsInvested = retirementYear - currentYear;
 
     let futureInvestmentValue = Math.round(initialInvestment * Math.pow((1 + annualInterestRate / 100),yearsInvested));
  
     futureInvestmentValue = futureInvestmentValue.toLocaleString();
     
-    let resultText = document.getElementById('resultText');
+    const resultText = document.getElementById('resultText');
     resultText.textContent = `Your investment will be worth $${futureInvestmentValue} by the time you retire in ${retirementYear}.`;
     resultText.hidden = false;  // Makes the result visible
 
